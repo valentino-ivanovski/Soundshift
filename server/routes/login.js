5c105d7e11090d5926b24d3306ddd1a2cba0de7e
Valentino Ivanovski <valentino.ivanovski@icloud.com>
2024-02-24 01:08:17 +0100

    Error message when incorrect credentials

--- server/routes/login.js
+++ server/routes/login.js
@@ -8,25 +8,26 @@ router.get('/', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
-    const { username, password } = req.body;
-  
-    // Check the username and password against the database
-    dbConn.checkUserCredentials(username, password, (err, user) => {
-      if (err) {
-        console.error("Error checking user credentials:", err);
-        res.status(500).send("Login failed. Please try again.");
-      } else if (!user) {
-        console.log("User not found or incorrect password");
-        res.status(401).send("Invalid username or password.");
-      } else {
-        console.log("User logged in successfully");
-  
-        // Set the user session
-        req.session.user = user;
+  const { username, password } = req.body;
 
-        res.redirect("/mainpage");
+  // Check the username and password against the database
+  dbConn.checkUserCredentials(username, password, (err, user) => {
+      if (err) {
+          console.error("Error checking user credentials:", err);
+          res.status(500).send("Login failed. Please try again.");
+      } else if (!user) {
+          console.log("User not found or incorrect password");
+          res.render('login', { error: "Invalid username or password." }); // Render the login page with an error message
+      } else {
+          console.log("User logged in successfully");
+
+          // Set the user session
+          req.session.user = user;
+
+          res.redirect("/mainpage");
       }
-    });
   });
+});
+
 
 module.exports = router; //exporting the module for users
