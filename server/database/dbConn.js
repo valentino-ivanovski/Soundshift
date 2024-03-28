dd4469f438ba5f53ff601a46017ba8896f10bae9
Valentino Ivanovski <valentino.ivanovski@icloud.com>
2024-03-28 04:26:13 +0100

    Added an arrow and a header to the comments div.

--- server/database/dbConn.js
+++ server/database/dbConn.js
@@ -3,7 +3,7 @@ const conn = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: 'SoundshiftTest'
+    database: "SoundshiftTest",
 });
 
 conn.connect((err) => {
@@ -11,11 +11,12 @@ conn.connect((err) => {
         console.error("ERROR: " + err);
         return;
     }
-    console.log('Database connection established');
+    console.log("Database connection established");
 });
 
 const insertUser = (username, email, password, callback) => {
-    const sql = "INSERT INTO usersNew (username, email, password) VALUES (?, ?, ?)";
+    const sql =
+        "INSERT INTO usersNew (username, email, password) VALUES (?, ?, ?)";
     conn.query(sql, [username, email, password], (err, result) => {
         callback(err, result);
     });
@@ -39,7 +40,8 @@ const checkUserCredentials = (username, password, callback) => {
 };
 
 const checkUsernameExists = (username, email, callback) => {
-    const sql = "SELECT COUNT(*) AS count FROM usersNew WHERE username = ? OR email = ?";
+    const sql =
+        "SELECT COUNT(*) AS count FROM usersNew WHERE username = ? OR email = ?";
     conn.query(sql, [username, email], (err, results) => {
         if (err) {
             callback(err, null);
@@ -52,7 +54,8 @@ const checkUsernameExists = (username, email, callback) => {
 
 //function to check if a song already exists in the database
 const checkSongExists = (title, artist, callback) => {
-    const sql = "SELECT COUNT(*) AS count FROM songsNew WHERE title = ? AND artist = ?";
+    const sql =
+        "SELECT COUNT(*) AS count FROM songsNew WHERE title = ? AND artist = ?";
     conn.query(sql, [title, artist], (err, results) => {
         if (err) {
             callback(err, null);
@@ -66,9 +69,15 @@ const checkSongExists = (title, artist, callback) => {
 //function to insert a new song into the database
 function insertSong(title, artist, userId, genre, explicit, callback) {
     // Generate links
-    const ytLink = `https://www.youtube.com/results?search_query=${encodeURIComponent(title)}%20${encodeURIComponent(artist)}`;
-    const spotifyLink = `https://open.spotify.com/search/${encodeURIComponent(title)}%20${encodeURIComponent(artist)}`;
-    const soundcloudLink = `https://soundcloud.com/search?q=${encodeURIComponent(title)}%20${encodeURIComponent(artist)}`;
+    const ytLink = `https://www.youtube.com/results?search_query=${encodeURIComponent(
+        title
+    )}%20${encodeURIComponent(artist)}`;
+    const spotifyLink = `https://open.spotify.com/search/${encodeURIComponent(
+        title
+    )}%20${encodeURIComponent(artist)}`;
+    const soundcloudLink = `https://soundcloud.com/search?q=${encodeURIComponent(
+        title
+    )}%20${encodeURIComponent(artist)}`;
 
     //prepare query and data
     const query = `
@@ -77,13 +86,22 @@ function insertSong(title, artist, userId, genre, explicit, callback) {
         VALUES 
             (?, ?, ?, ?, ?, ?, ?, NOW(), 0, 0, ?)
     `;
-    const data = [title, artist, genre, ytLink, spotifyLink, soundcloudLink, userId, explicit];
+    const data = [
+        title,
+        artist,
+        genre,
+        ytLink,
+        spotifyLink,
+        soundcloudLink,
+        userId,
+        explicit,
+    ];
 
     //execute query
     conn.query(query, data, (error, results) => {
         if (error) {
-            console.error('Error inserting song:', error);
-            callback('Error inserting song into the database');
+            console.error("Error inserting song:", error);
+            callback("Error inserting song into the database");
             return;
         }
         callback(null);
@@ -94,7 +112,7 @@ function insertSong(title, artist, userId, genre, explicit, callback) {
 function getRandomSong(songGenre, explicit, callback) {
     let sql = ``;
     if (explicit === false) {
-    sql = `
+        sql = `
         SELECT songsNew.*, usersNew.username 
         FROM songsNew 
         LEFT JOIN usersNew ON songsNew.user_id = usersNew.id 
@@ -102,7 +120,7 @@ function getRandomSong(songGenre, explicit, callback) {
         ORDER BY RAND() 
         LIMIT 1
     `;
-    }else{
+    } else {
         sql = `
         SELECT songsNew.*, usersNew.username 
         FROM songsNew 
@@ -124,7 +142,7 @@ function getRandomSong(songGenre, explicit, callback) {
 function getRandomSongAny(explicit, callback) {
     let sql = ``;
     if (explicit === false) {
-    sql = `
+        sql = `
         SELECT songsNew.*, usersNew.username, usersNew.location 
         FROM songsNew 
         LEFT JOIN usersNew ON songsNew.user_id = usersNew.id 
@@ -132,7 +150,7 @@ function getRandomSongAny(explicit, callback) {
         ORDER BY RAND() 
         LIMIT 1
     `;
-    }else{
+    } else {
         sql = `
         SELECT songsNew.*, usersNew.username, usersNew,location 
         FROM songsNew 
@@ -150,15 +168,23 @@ function getRandomSongAny(explicit, callback) {
 }
 
 function updateLocation(location, userId, callback) {
-    console.log('Updating location for user:', userId, 'New location:', location);
+    console.log(
+        "Updating location for user:",
+        userId,
+        "New location:",
+        location
+    );
 
     const sql = `UPDATE usersNew SET location = ? WHERE id = ?`;
     conn.query(sql, [location, userId], (err, results) => {
         if (err) {
-            console.error('Error executing SQL query:', err);
+            console.error("Error executing SQL query:", err);
             callback(err);
         } else {
-            console.log('Location set successfully. Affected rows:', results.affectedRows);
+            console.log(
+                "Location set successfully. Affected rows:",
+                results.affectedRows
+            );
             callback(null);
         }
     });
@@ -168,53 +194,64 @@ function incrementLikeCount(songId, callback) {
     const sql = `UPDATE songsNew SET like_count = like_count + 1 WHERE song_id = ?`;
     conn.query(sql, [songId], (err, results) => {
         if (err) {
-            console.error('Error executing SQL query:', err);
+            console.error("Error executing SQL query:", err);
             callback(err);
         } else {
-            console.log('Successfully incremented like count:', results.affectedRows);
+            console.log(
+                "Successfully incremented like count:",
+                results.affectedRows
+            );
             callback(null);
         }
     });
-};
+}
 
 function recordLike(songId, userId, callback) {
     const sql = `INSERT INTO liked_songs (song_id, user_id) VALUES (?, ?)`;
     conn.query(sql, [songId, userId], (err, results) => {
         if (err) {
-            console.error('Error liking SQL query recordLike():', err);
+            console.error("Error liking SQL query recordLike():", err);
             callback(err);
         } else {
-            console.log('Successfully recorded like:', results.affectedRows);
+            console.log("Successfully recorded like:", results.affectedRows);
             callback(null);
         }
     });
-};
+}
 
 function checkIfUserLiked(songId, userId, callback) {
     const sql = `SELECT * FROM liked_songs WHERE song_id = ? AND user_id = ?`;
     conn.query(sql, [songId, userId], (err, results) => {
         if (err) {
-            console.error('Error executing SQL query:', err);
+            console.error("Error executing SQL query:", err);
             callback(err);
         } else {
-            console.log('Successfully checked if user liked:', results.length > 0);
+            console.log(
+                "Successfully checked if user liked:",
+                results.length > 0
+            );
             callback(null, results.length > 0);
         }
     });
-};
+}
 
 function getCommentsBySongId(songId, callback) {
-    const sql = `SELECT * FROM comments WHERE song_id = ?`;
+    const sql =`SELECT comments.*, usersNew.username 
+                FROM comments 
+                INNER JOIN usersNew 
+                ON comments.user_id = usersNew.id 
+                WHERE song_id = 1`;
+
     conn.query(sql, [songId], (err, results) => {
         if (err) {
-            console.error('Error executing SQL query:', err);
+            console.error("Error executing SQL query:", err);
             callback(err);
         } else {
-            console.log('Successfully fetched comments:', results);
+            console.log("Successfully fetched comments:", results);
             callback(null, results);
         }
     });
-};
+}
 
 module.exports = {
     insertUser,
@@ -228,5 +265,5 @@ module.exports = {
     incrementLikeCount,
     recordLike,
     checkIfUserLiked,
-    getCommentsBySongId
+    getCommentsBySongId,
 };
