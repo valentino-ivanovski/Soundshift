af74a7e9485e58fbe862006a77b9b43a17fc0e6f
Valentino Ivanovski <valentino.ivanovski@icloud.com>
2024-04-28 06:01:24 +0200

    hashing implemented

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router()
 const dbConn = require('../database/dbConn');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
     console.log("login route reached");
@@ -8,25 +9,36 @@ router.get('/', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  //check the username and password against the database
-  dbConn.checkUserCredentials(username, password, (err, user) => {
-      if (err) {
-          console.error("Error checking user credentials:", err);
-          res.status(500).send("Login failed. Please try again.");
-      } else if (!user) {
-          console.log("User not found or incorrect password");
-          res.render('login', { error: "Invalid username or password." }); //render the login page with an error message
-      } else {
-          console.log("User logged in successfully");
+    // get the user from the database
+    dbConn.getUserByUsername(username, (err, user) => {
+        if (err) {
+            console.error("Error getting user:", err);
+            res.status(500).send("Login failed. Please try again.");
+        } else if (!user) {
+            console.log("User not found");
+            res.render('login', { error: "Invalid username or password." }); // render the login page with an error message
+        } else {
+            // check the entered password against the hashed password in the database
+            bcrypt.compare(password, user.password, function(err, result) {
+                if (err) {
+                    console.error("Error comparing passwords:", err);
+                    res.status(500).send("Login failed. Please try again.");
+                } else if (!result) {
+                    console.log("Incorrect password");
+                    res.render('login', { error: "Invalid username or password." }); // render the login page with an error message
+                } else {
+                    console.log("User logged in successfully");
 
-          //set the user session
-          req.session.user = user;
+                    // set the user session
+                    req.session.user = user;
 
-          res.redirect("/mainpage");
-      }
-  });
+                    res.redirect("/mainpage");
+                }
+            });
+        }
+    });
 });
 
 router.get('/mainpage', (req, res) => {
